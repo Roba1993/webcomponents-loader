c0b8bc5a9a6e49e34d5894a4fafe1e1f09496bd9
Robert Schütte <schuetterobert@gmail.com>
2016-11-25 21:31:33 +0100

    Added first version of the loader

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # webcomponents-loader
-Webpack loader for webcomponents
+Webpack loader for webcomponents. When you want to load webcomponents directly with webpack and package them together, you can take this loader. Simply load any `.html` file with 'require' or 'import'.
+
+Actual supported features:
+* Load .html files
+* Compile included javascript with babel
+* Compress the html/css/javascript with minify
+* Included javascript will be automatically executed on import
+
+## How to use
+Configure the loader in the `webpack.config.js` as following:
+```javascript
+module.exports = {
+    // ...
+    module: {
+        loaders: [
+          // html loader
+          {
+            test: /\.html$/,
+            loader: path.join(__dirname, "webcomponents-loader.js"),
+            query: {
+              // optinal parameter to use babel
+              babel: {
+                presets: 'es2015',
+                compact: true
+              },
+              // optinal parameter to use minify
+              minify: {
+                removeAttributeQuotes: true,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: true,
+                collapseWhitespace: true
+              }
+            }
+          }
+          // ...
+        ]
+    }
+};
+
+```
+
+## Known Bugs
+The `document.currentScript` and `document._currentScript` is nor working correctly. It will always guide to your `build.js` file/tag.
+* Instead of `document.currentScript` you can use the `currentWebcomponent` variable to get your actual tag reference.
+
+## Contribution
+Feel free to submit any bugfix or additional feature. If it's good documented and adds value i will accept your request.
---
+++ index.js
@@ -0,0 +1,68 @@
+let cheerio = require('cheerio');
+
+module.exports = function(content) {
+  // get the query (settings) as object
+  var query = JSON.parse(this.query.substr(1));
+
+  // put the content in a dom structure
+  let $ = cheerio.load(content);
+
+  // when babel options are set - use it
+  if(query.babel) {
+    // get all script tags
+    $('script').each(function(i, elem) {
+      // get the babel-core when it is defined
+      var babel = require("babel-core");
+      // transform the script with babel
+      var bblsrc = babel.transform($(this).text(), query.babel).code;
+      // replace the script with the converted one
+      $(this).text(bblsrc);
+    });
+  }
+
+  // put the result into a variable
+  var result = $.html();
+
+  // when minify options are set - minify
+  if(query.minify) {
+    // get the minify if required
+    var minify = require('html-minifier').minify;
+    // minify the complete file
+    result = minify(result, query.minify);
+  }
+
+  // replace all ' signs to be able to put the result into a variable
+  result = result.replace(/'/g, "\\'");
+
+  var js = `
+    // function to run code, to hide the variables and
+    // structure of the following code block
+    function run(code, node) {
+      var currentWebcomponent = node;
+      eval(code);
+    };
+
+    // get the head element of the main page
+    var link = document.querySelector('head');
+    // create a new link element
+    var element = document.createElement("link");
+    // set the import attribute to it
+    element.setAttribute('rel', 'import');
+    // create a shadow root inside of it
+    var shadow = element.createShadowRoot();
+    // set the result content into the shadow root
+    shadow.innerHTML = '${result}';
+    // put everything into the head
+    var res = link.appendChild(element);
+
+    // get all script tags and loop over them
+    var codes = shadow.querySelectorAll('script');
+    for(var i=0;i<codes.length;i++)
+    {
+      // execute each script tag
+      run(codes[i].text, shadow);
+    }
+  `;
+
+  return js;
+};
